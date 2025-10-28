9b8194a6afa28516cbaaacc4776545ecc7fa4bc7
zeroazulagencia <cristian.parada@zeroazul.com>
2025-10-27 22:58:42 -0500

    v18.22: Página de edición de cliente con agentes asociados

--- app/clientes/page.tsx
+++ app/clientes/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import { useClients, Client } from '@/utils/useClients';
 
 export default function Clientes() {
