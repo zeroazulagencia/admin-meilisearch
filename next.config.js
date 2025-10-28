edce40d9b8f31f9eb32a6854b8e3c069bd7c7cc7
zeroazulagencia <cristian.parada@zeroazul.com>
2025-10-28 11:30:18 -0500

    v18.33: Deshabilitar completamente cache

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0',
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
